b22f70c12f57486c0dfcd13f73732629f2944aa2
mohamed salah <mohamedsalah.owida@gmail.com>
2025-09-17 23:44:38 +0300

    Update Library Files

--- Library/STM32F446xx.h
+++ Library/STM32F446xx.h
@@ -4,29 +4,31 @@
 
 /************************************** Various  memories base addresses *************************/
 
-#define FLASH_BASE_ADDRESS		0x08000000U
-#define SRAM_BASE_ADDRESS		0x20000000U
-#define ROM_BASE_ADDRESS		0x1FFF0000U
+#define FLASH_BASE_ADDRESS		0x08000000UL
+#define SRAM_BASE_ADDRESS		0x20000000UL
+#define ROM_BASE_ADDRESS		0x1FFF0000UL
 
 
 /**************************************core peripheral base addresses *************************/
 
-#define SYSTIC_BASE_ADDRESS     0xE000E010U
+#define SYSTIC_BASE_ADDRESS     0xE000E010UL
+#define NVIC_BASE_ADDRESS       0xE000E100UL
+#define SCB_BASE_ADDRESS        0xE000E008UL
 
 
 
 
 /******************* AHB1 Peripheral Base Addresses *******************/
-#define GPIOA_BASE_ADDRESS		0x40020000U			
-#define GPIOB_BASE_ADDRESS      0x40020400U 
-#define GPIOC_BASE_ADDRESS      0x40020800U
-#define GPIOD_BASE_ADDRESS      0x40020C00U           
-#define GPIOE_BASE_ADDRESS      0x40021000U           
-#define GPIOF_BASE_ADDRESS      0x40021400U           
-#define GPIOG_BASE_ADDRESS      0x40021800U           
-#define GPIOH_BASE_ADDRESS      0x40021C00U   
-        
-#define RCC_BASE_ADDRESS		0x40023800U
+#define GPIOA_BASE_ADDRESS		0x40020000UL
+#define GPIOB_BASE_ADDRESS      0x40020400UL
+#define GPIOC_BASE_ADDRESS      0x40020800UL
+#define GPIOD_BASE_ADDRESS      0x40020C00UL
+#define GPIOE_BASE_ADDRESS      0x40021000UL
+#define GPIOF_BASE_ADDRESS      0x40021400UL
+#define GPIOG_BASE_ADDRESS      0x40021800UL
+#define GPIOH_BASE_ADDRESS      0x40021C00UL
+
+#define RCC_BASE_ADDRESS		0x40023800UL
 
 /******************* AHB2 Peripheral Base Addresses *******************/
 
@@ -49,6 +51,56 @@ typedef struct
 	volatile uint32_t CALIB;
 
 }Systic_regdef_t;
+
+
+/*******************NVIC Registers Definition Structures *******************/
+
+typedef struct
+{
+	volatile uint32_t ISER[8];
+	volatile uint32_t RESERVED1[24];
+	volatile uint32_t ICER[8];
+	volatile uint32_t RESERVED2[24];
+	volatile uint32_t ISPR[8];
+	volatile uint32_t RESERVED3[24];
+	volatile uint32_t ICPR[8];
+	volatile uint32_t RESERVED4[24];
+	volatile uint32_t IABR[8];
+	volatile uint32_t RESERVED5[56];
+	volatile uint8_t IPR[240];
+	volatile uint32_t RESERVED6[644];
+	volatile uint32_t STIR;
+
+}NVIC_Regdef_t;
+
+/*******************SCB Registers Definition Structures *******************/
+
+typedef  struct
+{
+	volatile uint32_t ACTLR;					/*Auxiliary Control Register*/
+	volatile uint32_t RESERVED0[829];           /*Reserved for 3316 bytes offset*/
+	volatile uint32_t CPUID;                    /*CPU ID Base Register*/
+	volatile uint32_t ICSR;                     /*Interrupt Control and State Register*/
+	volatile uint32_t VTOR;                     /*Vector Table Offset Register*/
+	volatile uint32_t AIRCR;                    /*Application Interrupt and Reset Control Register*/
+	volatile uint32_t SCR;                      /*System Control Register*/
+	volatile uint32_t CCR;                      /*Configuration & Control Register*/
+	volatile uint32_t SHPR[3];                  /*System Handler Priority Registers*/
+	volatile uint32_t SHCRS;                    /*System Handler Control and State Register*/
+	union                                       /*Union because the following two Regs. share the same address*/
+	{
+		volatile uint32_t CFSR;                 /*Configurable Fault Status Register*/
+		volatile uint32_t MMSR;                 /*MemManage Fault Status Register*/
+	};
+	volatile uint8_t BFSR;                      /*BusFault Status Register*/
+	volatile uint16_t UFSR;                     /*UsageFault Status Register*/
+	volatile uint32_t HFSR;                     /*HardFault Status Register*/
+	volatile uint32_t RESERVED1;                /*Reserved for 4 bytes offset*/
+	volatile uint32_t MMAR;                     /*MemManage Fault Address Register*/
+	volatile uint32_t BFAR;                     /*Bus Fault Address Register*/
+	volatile uint32_t AFSR;                     /*Auxiliary Fault Status Register*/
+
+}SCB_Regdef_t;
 
 
 /******************* RCC Registers Definition Structures *******************/
@@ -88,8 +140,8 @@ typedef struct
 	volatile uint32_t DCKCFGR;                  /*RCC Dedicated Clock configuration Register*/
 	volatile uint32_t CKGATENR;                 /*RCC Clocks Gated Enable Register*/
 	volatile uint32_t DCKCFGR2;                 /*RCC Dedicated Clocks Configuration register 2*/
-	
-	
+
+
 }RCC_RegDef_t;
 
 /******************* GPIO Registers Definition Structures *******************/
@@ -126,12 +178,12 @@ typedef struct
 
 /******************* SYSTIC Peripheral Definition *******************/
 
-#define Systic     ((Systic_regdef_t *) SYSTIC_BASE_ADDRESS )
+#define Systic     ((Systic_regdef_t *) SYSTIC_BASE_ADDRESS)
 
+/******************* NVIC Peripheral Definition *******************/
+#define NVIC       (( NVIC_Regdef_t *) NVIC_BASE_ADDRESS)
 
-
-
-
+#define SCB        ((SCB_Regdef_t * )  SCB_BASE_ADDRESS )
 
 
 #endif
